4a4b031c7bf8ab372ff6e5cff5ef681d677f92a0
Raghu Gopalakrishnan <graghunair@gmail.com>
2024-02-18 13:20:08 +0300

    Update DDL_Tables.sql

--- DDL_Tables.sql
+++ DDL_Tables.sql
@@ -51,56 +51,57 @@ GO
 CREATE SCHEMA [load] AUTHORIZATION [dbo]
 GO
 CREATE TABLE [load].[tblSQL_Server_Details](
-				[SQL_Server_Instance] [nvarchar](128) NOT NULL,
-				[SQL_Server_IP] [varchar](48) NULL,
-				[SQL_Server_Port] [int] NOT NULL,
-				[Server_Name] [sysname] NULL,		
-				[Server_Host] [sysname] NULL,
-				[Server_Domain] [varchar](256) NULL,
-				[SQL_Server_Edition] [nvarchar](128) NULL,
-				[SQL_Server_Version] [nvarchar](128) NULL,
-				[OS_Version] [nvarchar](128) NULL,
-				[Is_Clustered] [bit] NULL,
-				[Is_Hadr_Enabled] [bit] NULL,
-				[User_Databases_Count] [smallint] NULL,
-				[Databases_Data_Size_GB] [decimal](10, 2) NULL,
-				[Databases_TLog_Size_GB] [decimal](10, 2) NULL,
-				
-				[CPU_Count] [smallint] NULL,
-				[Physical_Memory_GB] [decimal](10, 2) NULL,
-				[Committed_Target_GB] [decimal](10, 2) NULL,
-				[Collation] [nvarchar](128) NULL,
-				[Last_Full_Backup_Timestamp] DATETIME NULL,
-				[Last_Full_Backup_Database_Name] SYSNAME,
-				[Last_TLog_Backup_Timestamp] DATETIME NULL,
-				[Last_TLog_Backup_Database_Name] SYSNAME,
-				[Instance_Default_Data_Path] nvarchar(128) NULL,
-				[Instance_Default_TLog_Path] nvarchar(128) NULL,
-				[SQL_Server_Service_Account] [nvarchar](256) NULL,
-				[SQL_Server_Start_Time] [datetime] NULL,
-				[Ad_Hoc_Distributed_Queries] [bit] NULL,
-				[Backup_Compression_Default] [bit] NULL,
-				[CLR_Enabled] [bit] NULL,
-				[Filestream_Access_Level] [tinyint] NULL,
-				[MAXDOP] [tinyint] NULL,
-				[Optimize_for_ad_hoc_Workloads] [bit] NULL,
-				[Xp_Cmdshell] [bit] NULL,
-				[File_System_Storage] [xml] NULL,
-				[Date_Captured] [datetime] NOT NULL,
-			 CONSTRAINT [PK_Load_tblSQL_Server_Details] PRIMARY KEY NONCLUSTERED 
-			(
-				[SQL_Server_Instance] ASC
-			)) 
+      [SQL_Server_Instance] [nvarchar](128) NOT NULL,
+      [SQL_Server_IP] [varchar](48) NULL,
+      [SQL_Server_Port] [int] NOT NULL,
+      [Server_Name] [sysname] NULL,
+      [Server_Host] [sysname] NULL,
+      [Server_Domain] [varchar](256) NULL,
+      [SQL_Server_Edition] [nvarchar](128) NULL,
+      [SQL_Server_Version] [nvarchar](128) NULL,
+      [OS_Version] [nvarchar](128) NULL,
+      [Is_Clustered] [bit] NULL,
+      [Is_Hadr_Enabled] [bit] NULL,
+      [User_Databases_Count] [smallint] NULL,
+      [Databases_Data_Size_GB] [decimal](10, 2) NULL,
+      [Databases_TLog_Size_GB] [decimal](10, 2) NULL,
+      [CPU_Count] [smallint] NULL,
+      [Physical_Memory_GB] [decimal](10, 2) NULL,
+      [Committed_Target_GB] [decimal](10, 2) NULL,
+      [Collation] [nvarchar](128) NULL,
+      [Last_Full_Backup_Timestamp] [datetime] NULL,
+      [Last_Full_Backup_Database_Name] [sysname] NOT NULL,
+      [Last_TLog_Backup_Timestamp] [datetime] NULL,
+      [Last_TLog_Backup_Database_Name] [sysname] NOT NULL,
+      [Instance_Default_Data_Path] [nvarchar](128) NULL,
+      [Instance_Default_TLog_Path] [nvarchar](128) NULL,
+      [SQL_Server_Service_Account] [nvarchar](256) NULL,
+      [SQL_Server_Start_Time] [datetime] NULL,
+      [Ad_Hoc_Distributed_Queries] [bit] NULL,
+      [Backup_Compression_Default] [bit] NULL,
+      [CLR_Enabled] [bit] NULL,
+      [Filestream_Access_Level] [tinyint] NULL,
+      [MAXDOP] [tinyint] NULL,
+      [Optimize_for_ad_hoc_Workloads] [bit] NULL,
+      [Xp_Cmdshell] [bit] NULL,
+      [File_System_Storage] [xml] NULL,
+      [AG_Properties] [xml] NULL,
+      [Date_Captured] [datetime] NOT NULL,
+ CONSTRAINT [PK_Load_tblSQL_Server_Details] PRIMARY KEY NONCLUSTERED
+(
+      [SQL_Server_Instance] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) 
+) 
 GO
 
-ALTER TABLE [load].[tblSQL_Server_Details] 
-ADD CONSTRAINT [DF_load_tblSQL_Servers_Details_Date_Created] 
-DEFAULT (getdate()) FOR [Date_Captured]
+ALTER TABLE [load].[tblSQL_Server_Details] ADD  CONSTRAINT [DF_load_tblSQL_Servers_Details_Date_Created]  DEFAULT (getdate()) FOR [Date_Captured]
 GO
-ALTER TABLE [load].[tblSQL_Server_Details] 
-ADD CONSTRAINT [FK_tblSQL_Server_Details] 
-FOREIGN KEY ([SQL_Server_Instance])     
-REFERENCES [inventory].[tblSQL_Servers] ([SQL_Server_Instance]) 
+
+ALTER TABLE [load].[tblSQL_Server_Details]  WITH CHECK ADD  CONSTRAINT [FK_tblSQL_Server_Details] FOREIGN KEY([SQL_Server_Instance])
+REFERENCES [inventory].[tblSQL_Servers] ([SQL_Server_Instance])
+GO
+
+ALTER TABLE [load].[tblSQL_Server_Details] CHECK CONSTRAINT [FK_tblSQL_Server_Details]
 GO
 
 DROP VIEW IF EXISTS [dbo].[vwSQL_Server_Details]
